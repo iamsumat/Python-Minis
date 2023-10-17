18655cd04f89591cf6501bfb15a7623c1f770b9f
sumat.bedi <sumat.bedi@st.ovgu.de>
2023-10-17 19:45:33 +0200

    password manager file

---
+++ password_manager.py
@@ -0,0 +1,22 @@
+master_pwd = input("What is the master password? ")
+
+def view():
+    pass
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        
+
+while True:
+    mode = input("Would you like to add a new password, or view existing ones? (view, add, q to quit) ").lower()
+    if mode == "q": 
+        break
+
+    if mode == "view": view()
+    elif mode == "add": add()
+    else:
+        print("Invalid mode.")
+        continue
